a638f5dd0dca8937bf8d476e4bb483ca7c7384fe
Bimigijoo <bimigijo1212@gmail.com>
2026-07-14 22:54:23 +0530

    Implemented add lab feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import AddLog from "./components/AddLog";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<AddLog />} />
+
+    </Routes>
     </>
   )
 }
